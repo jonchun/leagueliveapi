6a5bbef5e5106f6778e1d0e485e672874879a4f2
Jonathan Chun <git@jonathanchun.com>
2021-07-08 22:59:34 -0400

    update example for New()

--- api_test.go
+++ api_test.go
@@ -12,5 +12,6 @@ func ExampleNew() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(data)
+	fmt.Printf("%#v\n", data)
+	// Output: leagueliveapi.GameData{GameMode:"PRACTICETOOL", GameTime:13.315361022949219, MapName:"Map11", MapNumber:11, MapTerrain:"Default"}
 }
